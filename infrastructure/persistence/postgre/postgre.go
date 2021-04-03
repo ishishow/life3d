403dd530e7f3383ac32aa9538684b32ce1d986eb
ishishow <ishikawa.sho.infratop@gmail.com>
2021-04-03 22:41:34 +0900

    Add: initial config

--- infrastructure/persistence/postgre/postgre.go
+++ infrastructure/persistence/postgre/postgre.go
@@ -1,12 +1,35 @@
 package postgre
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+)
+
 const driverName = "postgre"
 
-//type SQLHandler struct {
-//	Conn *sql.DB
-//}
+type SQLHandler struct {
+	Conn *sql.DB
+}
 
-//func NewSQLHandler() SQLHandler {
-//
-//
-//}
+func NewSQLHandler() SQLHandler {
+	// config
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	database := os.Getenv("POSTGRES_DATABASE")
+
+	// 接続情報は以下のように指定する.
+	// user:password@tcp(host:port)/database
+	conn, err := sql.Open(driverName,
+		fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, database))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return SQLHandler{
+		Conn: conn,
+	}
+}
