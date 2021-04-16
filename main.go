a56ccc283e9d9e7209e7697b595b741d2228858d
ishishow <ishikawa.sho.infratop@gmail.com>
2021-04-16 14:19:43 +0900

    format heroku production

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"lifegame/presenter/http/server"
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 var (
@@ -15,17 +11,18 @@ var (
 )
 
 func init() {
-	//flag.StringVar(&addr, "addr", ":"+os.Getenv("PORT"), "tcp host:port to connect")
-	flag.StringVar(&addr, "addr", ":8080", "tcp host:port to connect")
+	flag.StringVar(&addr, "addr", ":"+os.Getenv("PORT"), "tcp host:port to connect")
+	//flag.StringVar(&addr, "addr", ":8080", "tcp host:port to connect")
 	flag.Parse()
 }
 
 func main() {
 
-	err := godotenv.Load(fmt.Sprintf("./%s.env", os.Getenv("GO_ENV")))
-	if err != nil {
-		log.Fatalf("getenv is failed! :%v", err)
-	}
+	// local
+	//err := godotenv.Load(fmt.Sprintf("./%s.env", os.Getenv("GO_ENV")))
+	//if err != nil {
+	//	log.Fatalf("getenv is failed! :%v", err)
+	//}
 
 	server.Serve(addr)
 }
